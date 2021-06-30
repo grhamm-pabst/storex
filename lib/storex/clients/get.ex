52d4b335ee835b649509ad37462fdc37629339ae
Grhamm Pabst <grhammpabst456@gmail.com>
2021-06-30 12:51:38 -0300

    Get client by email and tests

--- lib/storex/clients/get.ex
+++ lib/storex/clients/get.ex
@@ -7,4 +7,11 @@ defmodule Storex.Clients.Get do
       %Client{} = client -> {:ok, client}
     end
   end
+
+  def by_email(email) do
+    case Repo.get_by(Client, email: email) do
+      nil -> {:error, Error.build(:not_found, "This Client doesn't exist")}
+      %Client{} = client -> {:ok, client}
+    end
+  end
 end
--- test/storex/clients/get_test.exs
+++ test/storex/clients/get_test.exs
@@ -35,4 +35,34 @@ defmodule Storex.Clients.GetTest do
       assert expected_response == response
     end
   end
+
+  describe "by_email/1" do
+    test "when the client exists, returns the client" do
+      %Client{email: email} = insert(:client)
+
+      response = Get.by_email(email)
+
+      assert {:ok,
+              %Storex.Client{
+                email: "grhammpabst@email.com",
+                id: "5484b227-0f8f-4e84-ab01-41fd7c4c43dc",
+                inserted_at: _in_date,
+                name: "Grhamm Pabst",
+                password: nil,
+                password_hash: nil,
+                updated_at: _up_date
+              }} = response
+    end
+
+    test "when the client with the given email doesn't exists, returns an error" do
+      email = "grh@c.om"
+
+      response = Get.by_email(email)
+
+      expected_response =
+        {:error, %Storex.Error{result: "This Client doesn't exist", status: :not_found}}
+
+      assert expected_response == response
+    end
+  end
 end
